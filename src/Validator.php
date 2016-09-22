fa3fb50eae29f35ad7c34be11fd2e87646bed035
Johan Björk <johanimon@gmail.com>
2016-09-22 18:04:19 +0200

    Add Validator class

---
+++ src/Validator.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johan
+ * Date: 2016-09-22
+ * Time: 17:27
+ */
+
+namespace Vinnia\Upsales;
+
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\ValidatorBuilder;
+
+class Validator
+{
+
+    /**
+     * @var ValidatorInterface
+     */
+    private $backend;
+
+    function __construct(ValidatorInterface $backend)
+    {
+        $this->backend = $backend;
+    }
+
+    public static function make(): self
+    {
+        return new self((new ValidatorBuilder)->getValidator());
+    }
+
+    protected function getProductConstraint(): Constraint
+    {
+        return new Collection([
+            'allowExtraFields' => true,
+            'fields' => [
+                'id' => new Type('integer'),
+            ],
+        ]);
+    }
+
+    protected function getOrderRowConstraint(): Constraint
+    {
+        return new Collection([
+            'allowExtraFields' => true,
+            'fields' => [
+                'product' => $this->getProductConstraint(),
+                'price' => new Type('integer'),
+                'quantity' => new Type('integer'),
+            ],
+        ]);
+    }
+
+    public function validateOrder(array $data): ConstraintViolationListInterface
+    {
+        return $this->backend->validate($data, new Collection([
+            'allowExtraFields' => true,
+            'fields' => [
+                'description' => new Type('string'),
+                'date' => new DateTime([
+                    'format' => 'Y-m-d\TH:i:sP', // ISO 8601 eg 2016-09-22T17:50:00+02:00
+                ]),
+                'user' => new Type('integer'),
+                'stage' => new Type('integer'),
+                'probability' => new Type('integer'),
+                'client' => new Type('integer'),
+                'orderRow' => new All($this->getOrderRowConstraint()),
+            ],
+        ]));
+    }
+
+}
